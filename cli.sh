a25d248157cc5227e81a181eaaf3ca3765640eec
bbangg <bbanggdev@gmail.com>
2024-03-22 09:07:06 +0300

    fix+chore: cli, simplify shard config

--- cli.sh
+++ cli.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ "$1" != "start" ] && [ "$1" != "stop" ] && [ "$1" != "purge" ]; then
     echo "Invalid argument."
     echo "./cli.sh [start|stop|purge] [master|shard]"
--- scripts/init-shard.sh
+++ scripts/init-shard.sh
@@ -2,22 +2,22 @@
 
 mongosh <<EOF
 var config = {
-    "_id": "rs-shard-03",
+    "_id": "rs-shard-01",
     "version": 1,
     "members": [
         {
             "_id": 0,
-            "host": "shard03-a:27017",
+            "host": "shard-01-a:27017",
 			"priority": 1
         },
         {
             "_id": 1,
-            "host": "shard03-b:27017",
+            "host": "shard-01-b:27017",
 			"priority": 0.5
         },
         {
             "_id": 2,
-            "host": "shard03-c:27017",
+            "host": "shard-01-c:27017",
 			"priority": 0.5
         }
     ]
